a91f65fb1681cffb1b5202c27e76ae6b317d5b56
leo.nguyentuong <leo.nguyentuong@gmail.com>
2024-03-08 11:49:40 +0100

    front: affichage des restaurant

--- publicApp/src/screens/Home.tsx
+++ publicApp/src/screens/Home.tsx
@@ -2,40 +2,101 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { NavigationProp, RouteProp } from '@react-navigation/native';
 const { backendUrl } = require('../config.ts');
-import { View, ScrollView, Text, StyleSheet, Button } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, Button, PermissionsAndroid } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+
+const requestLocationPermission = async () => {
+    try {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+                title: 'Geolocation Permission',
+                message: 'Can we access your location?',
+                buttonNeutral: 'Ask Me Later',
+                buttonNegative: 'Cancel',
+                buttonPositive: 'OK',
+            },
+        );
+        if (granted === 'granted') {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (err) {
+        return false;
+    }
+};
+
 
 export default function Home({ route, navigation }: { route: RouteProp<any>, navigation: NavigationProp<any> }) {
     const token = route.params?.token;
     const [restaurants, setRestaurants] = useState<any[]>([]);
+    const [location, setLocation] = useState<any>(null);
 
-    useEffect(() => {
-        axios.get(`${backendUrl}/restaurants/2/42/0.01`, { ///to do: get the alt et long  
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-            .then(response => {
-                setRestaurants(response.data.restaurants)
-            })
-            .catch(error => {
-                console.error('Error fetching restaurants:', error);
+    interface Position {
+        coords: {
+            latitude: number;
+            longitude: number;
+        }
+    }
+    
+    const getLocation = () => {
+        return new Promise<Position>((resolve, reject) => {
+            requestLocationPermission().then(result => {
+                if (result) {
+                    Geolocation.getCurrentPosition(
+                        position => {
+                            resolve(position);
+                        },
+                        error => {
+                            console.error('Geolocation error:', error.code, error.message);
+                            reject(error);
+                        },
+                        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+                    );
+                } else {
+                    reject(new Error('Permission denied'));
+                }
+            }).catch(error => {
+                console.error('Error getting location:', error);
+                reject(error);
             });
+        });
+    };
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const location: Position = await getLocation();
+
+                const response = await axios.get(`${backendUrl}/restaurants/${location.coords.longitude}/${location.coords.latitude}/0.001`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                setRestaurants(response.data.restaurants);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+    
+        fetchData();
     }, [token]);
     return (
-        <ScrollView  contentContainerStyle={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
             <Text >Email: { }</Text>
             <View style={styles.container}>
-            <View>
-                <Text style={styles.header}>Liste des restaurants:</Text>
-                {restaurants.map(restaurant => (restaurant.tags.name)? (
-                    <View key={restaurant.id} style={styles.restaurantContainer}>
-                        <Button color="#1355A2" title={restaurant.tags.name}/>
-                    </View>
-                ): null
-                
-                )}
+                <View>
+                    <Text style={styles.header}>Liste des restaurants:</Text>
+                    {restaurants.map(restaurant => (restaurant.tags.name) ? (
+                        <View key={restaurant.id} style={styles.restaurantContainer}>
+                            <Button color="#1355A2" title={restaurant.tags.name} />
+                        </View>
+                    ) : null
+
+                    )}
+                </View>
             </View>
-        </View>
         </ScrollView>
     );
 }
